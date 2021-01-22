5b5f35f1dbf0788857378253483a9a3e806fa635
Matheus Reis <theus.h.ass.reis@gmail.com>
2021-01-21 21:17:02 -0300

    print numbers 1 to 20

---
+++ list_e1/e1_02/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int i;
+
+    for (i = 1; i <= 20; i++) {
+        printf("%d\n", i);
+    }
+}
+
+/*
+E1_02) Faça um programa que apresente os números de 1 a 20, cada número em uma linha.
+
+*/
