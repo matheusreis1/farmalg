93ef186dbbc5a3b741b25a032db495401c0ce7aa
Matheus Reis <theus.h.ass.reis@gmail.com>
2021-01-21 21:24:44 -0300

    show in desc order numbers from 20 to 1

---
+++ list_e1/e1_04/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int i;
+
+    for (i = 20; i >= 1; i--) {
+        printf("%d\n", i);
+    }
+}
+
+/*
+E1_04) Faça um programa que apresente os números de 20 a 1 (em ordem decrescente), um número em cada linha.
+
+*/
