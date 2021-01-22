b272c2ee874284f9b0524a66c9ed09954ea5664a
Matheus Reis <theus.h.ass.reis@gmail.com>
2021-01-21 21:19:04 -0300

    print even numbers from 0 to 20

---
+++ list_e1/e1_03/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int i;
+
+    for (i = 0; i <= 20; i++) {
+        if ( i % 2 == 0 ) { 
+            printf("%d\n", i);
+        }
+    }
+}
+
+/*
+E1_03) Faça um programa que apresente os números de pares de 0 a 20, cada número em uma linha.
+
+*/
