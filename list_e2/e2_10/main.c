d915bd4244a630b8b08e7af9c309b10071e7b0af
Matheus Reis <theus.h.ass.reis@gmail.com>
2021-03-09 20:52:08 -0300

    even or odd

---
+++ list_e2/e2_10/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+void even_or_odd(int number);
+
+int main() {
+    int number = 1;
+
+    while (number) {
+        scanf("%d", &number);
+        number ? even_or_odd(number) : "";
+    }
+}
+
+void even_or_odd(int number) {
+    number % 2 == 0 ? printf("%d par\n", number) : printf("%d impar\n", number);
+}
+
+/*
+E10) Faça um programa que leia uma lista de números inteiros terminados por 0, para cada número lido o programa deve informar o número e se o número é par ou ímpar. Não utilize vetores/matrizes.
+
+output
+N classificação
+
+Onde N é o número lido e classificação deve ser "par" ou "impar" sem acentos.
+*/
