951cb4be966bab02ab4c1399da2e819df277d75f
Matheus Reis <theus.h.ass.reis@gmail.com>
2021-03-09 21:26:30 -0300

    read 3 numbers and use bitwise operators to get ASC order of them

---
+++ list_e2/e2_11/main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+void swap(int *x, int *y);
+
+int main() {
+    int quantity_of_numbers = 3;
+    int i, j;
+    int numbers[quantity_of_numbers];
+    int numbers_array_length = quantity_of_numbers - 1;
+
+    for (i = 0; i < quantity_of_numbers; i++) {
+        scanf("%d", &numbers[i]);
+    }
+
+    for (i = 0; i < numbers_array_length; i++) {
+        for (j = 0; j < numbers_array_length - i; j++) {
+            if (numbers[j] > numbers[j + 1]) {
+                swap(&numbers[j], &numbers[j+1]);
+            }
+        }
+    }
+
+    for (i = 0; i < quantity_of_numbers; i++) {
+        printf("%d\n", numbers[i]);
+    }
+}
+
+void swap(int *x, int *y) {
+    *x = *x ^ *y;
+    *y = *x ^ *y;
+    *x = *x ^ *y;
+}
+
+/*
+E11) Faça um programa que leia três números e os apresente em ordem crescente. Imprima um número em cada linha. Não apresente texto para a leitura dos três números.
+
+
+*/
